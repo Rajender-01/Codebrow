5ae8a38cb00b8a3c367778af52200ecb426656bc
Rajender Kumar <ft.rajender@gmail.com>
2025-03-04 22:04:38 +0530

    feat: add vertical Swiper slider with pagination and autoplay

---
+++ src/components/SwiperSlider/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "@/app/globals.css";
+
+// import required modules
+import { Pagination, Autoplay } from "swiper/modules";
+
+const SwiperSlider = () => {
+  return (
+    <Swiper
+      direction={"vertical"}
+      pagination={{
+        clickable: true,
+      }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
+      modules={[Pagination, Autoplay]}
+      className="mySwiper"
+    >
+      <SwiperSlide className="bg-red-500">Slide 1</SwiperSlide>
+      <SwiperSlide className="bg-blue-500">Slide 2</SwiperSlide>
+      <SwiperSlide className="bg-green-500">Slide 3</SwiperSlide>
+      <SwiperSlide className="bg-teal-500">Slide 9</SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default SwiperSlider;
