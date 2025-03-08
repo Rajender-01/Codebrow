b8cc3280703c575f0ec520d4a5c61f5ac59f9a40
Rajender Kumar <ft.rajender@gmail.com>
2025-03-08 14:35:46 +0530

    feat: update NewsletterBanner styling
    - Added container class for consistent width
    - Improved text color using CSS variables
    - Added padding for better spacing

--- src/components/NewsletterBanner/NewsletterBanner.tsx
+++ src/components/NewsletterBanner/NewsletterBanner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NewsletterBanner = () => {
   return (
-    <div className="w-full max-w-[1152px] mx-auto text-[var(--gray-font-color)] py-4 text-center">
+    <div className="w-full container mx-auto h-auto text-[var(--gray-font-color)] py-4 text-center">
       <p>
         Subscribe to our newsletter to get the latest updates and promotions.
       </p>
