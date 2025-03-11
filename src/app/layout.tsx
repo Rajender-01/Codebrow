034f507b9a001ae0422feaabacae862cac2f6cdd
Rajender Kumar <ft.rajender@gmail.com>
2025-03-11 23:58:02 +0530

    feat: added the navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/app/styles/globals.css";
 import Header from "@/components/layout/Header/Header";
+import NewsletterBanner from "@/components/sections/NewsletterBanner";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -26,6 +27,7 @@ export default function RootLayout({
         className={`${inter.className} bg-gray-100 dark:bg-[var(--dark-gray)] text-gray-900 dark:text-gray-100 min-h-screen`}
       >
         <Header />
+        <NewsletterBanner />
         {children}
         {/* footer */}
       </body>
